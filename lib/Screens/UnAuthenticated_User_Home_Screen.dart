af978b28fe8a0b65ae1991f71bf55bd78c40ae6d
Omar Nouh <omarnouh412@gmail.com>
2020-12-27 18:06:15 +0200

    Adding firebase configurations & Project structure

---
+++ lib/Screens/UnAuthenticated_User_Home_Screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class UnAuthenticatedHomeScreen extends StatefulWidget {
+  @override
+  _UnAuthenticatedHomeScreenState createState() =>
+      _UnAuthenticatedHomeScreenState();
+}
+
+class _UnAuthenticatedHomeScreenState extends State<UnAuthenticatedHomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
