5f3234db3260ec835baa6ed2a4690563009e2317
Omar Nouh <omarnouh412@gmail.com>
2021-01-05 16:31:45 +0200

    Creating Firebase function to push notificatons in vendor app when reserving new seat

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,19 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const admin = require('firebase-admin');
+
+admin.initializeApp();
+
+exports.Function = functions.firestore.document('Customer Notifications/{docID}').onCreate(
+    (snapshot, context) => {
+        return admin.messaging().sendToTopic('VendorNotfication',
+        {
+            notification : {
+                title : "New seats reserved !",
+                body : "a user reserved new seats, view it now.",
+                clickAction : 'FLUTTER_NOTIFICATION_CLICK'
+            }
+        }
+        );
+    }
+);
